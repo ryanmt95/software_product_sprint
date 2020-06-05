2818efa9c9ea5ec9ce01b269e2aca5d89b697217
Ryan M Tan <ryan.m.tan95@gmail.com>
2020-06-05 12:45:37 +0800

    included hover effects for header

--- portfolio/src/main/webapp/assets/javascript/script.js
+++ portfolio/src/main/webapp/assets/javascript/script.js
@@ -15,14 +15,20 @@
 /**
  * Adds a random greeting to the page.
  */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+// function addRandomGreeting() {
+//   const greetings =
+//       ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
 
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+//   // Pick a random greeting.
+//   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
 
-  // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
-}
+//   // Add it to the page.
+//   const greetingContainer = document.getElementById('greeting-container');
+//   greetingContainer.innerText = greeting;
+// }
+
+$(document).ready(function(){
+
+    // jQuery methods go here...
+
+});
