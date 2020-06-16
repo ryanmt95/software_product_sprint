6e508c2dd5ed099e72c4dd0c4b2a852bde7fdf1d
ryan <ryan.m.tan95@gmail.com>
2020-06-16 14:17:09 +0800

    implemented datastore functionality in contactformservlet

--- portfolio/src/main/java/com/google/sps/servlets/ContactFormServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/ContactFormServlet.java
@@ -7,26 +7,43 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.gson.Gson;
 
 /** Servlet that processes text. */
 @WebServlet("/contact_form")
 public final class ContactFormServlet extends HttpServlet {
-    
-    private Map<String,String> form = new HashMap<String, String>();
-    
+
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
     // Get input from form
-    form.put("fname", getParameter(request, "fname", ""));
-    form.put("lname", getParameter(request, "lname", ""));
-    form.put("email", getParameter(request, "email", ""));
-    form.put("mobile", getParameter(request, "mobile", ""));
-    form.put("message", getParameter(request, "message", ""));
+    String fname = getParameter(request, "fname", "");
+    String lname = getParameter(request, "lname", "");
+    String email = getParameter(request, "email", "");
+    String mobile = getParameter(request, "mobile", "");
+    String message = getParameter(request, "message", "");
 
-    // Respond with the result.
-    response.setContentType("text/html;");
-    response.getWriter().println(form);
+    // // Respond with the result.
+    // response.setContentType("text/html;");
+    // response.getWriter().println(form);
+
+    Entity form_entity = new Entity("contact_form");
+    form_entity.setProperty("fname", fname);
+    form_entity.setProperty("lname", lname);
+    form_entity.setProperty("email", email);
+    form_entity.setProperty("mobile", mobile);
+    form_entity.setProperty("message", message);
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    datastore.put(form_entity);
+
+    response.sendRedirect("/contact.html");
   }
 
   /**
@@ -39,5 +56,39 @@ public final class ContactFormServlet extends HttpServlet {
       return defaultValue;
     }
     return value;
+  }
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    Query query = new Query("contact_form");
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    PreparedQuery results = datastore.prepare(query);
+
+    List<Map<String, String>> users = new ArrayList<>();
+    for (Entity entity : results.asIterable()) {
+      long id = entity.getKey().getId();
+
+      Map<String, String> user = new HashMap<String, String>();
+
+      String fname = (String) entity.getProperty("fname");
+      String lname = (String) entity.getProperty("lname");
+      String email = (String) entity.getProperty("email");
+      String mobile = (String) entity.getProperty("mobile");
+      String message = (String) entity.getProperty("message");
+
+      user.put("fname", fname);
+      user.put("lname", lname);
+      user.put("email", email);
+      user.put("mobile", mobile);
+      user.put("message", message);
+
+      users.add(user);
+    }
+
+    Gson gson = new Gson();
+
+    response.setContentType("application/json;");
+    response.getWriter().println(gson.toJson(users));
   }
 }
