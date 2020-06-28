74ee9d5392e6efd5f9761cf663f0924b19006baf
ryan <ryan.m.tan95@gmail.com>
2020-06-28 10:16:20 +0800

    ammendments

--- walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -24,39 +24,36 @@ import java.lang.Math;
 
 public final class FindMeetingQuery {
 
-  private ArrayList<TimeRange> meetings = new ArrayList<TimeRange>();
-  private ArrayList<TimeRange> timeTable = new ArrayList<TimeRange>();
-  private int meetingDuration;
-  private Collection<String> attendees;
-
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-
-    meetingDuration = (int) request.getDuration();
-    attendees = request.getAttendees();
+    
+    int meetingDuration = (int) request.getDuration();
+    Collection<String> attendees = request.getAttendees();
     
     // Edge Cases
     // 1. when requested duration exceeds one day 
     // 2. there are no attendees requested to the meeting
-    if (request.getDuration() > (60*24)) {
+    if (meetingDuration > (60*24)) {
         return Arrays.asList();
-    } else if (request.getAttendees().size() == 0) {
+    } else if (attendees.size() == 0) {
         return Arrays.asList(TimeRange.WHOLE_DAY);
     }
 
-    constructTimeTable(events);
+    List<TimeRange> timeTable = constructTimeTable(events, attendees);
 
     // when there are no timeslots where attendees need to attend events
     if (timeTable.size() == 0) {
         return Arrays.asList(TimeRange.WHOLE_DAY);
     }
     
-    findMeetingSlots(timeTable);
+    List<TimeRange> meetings= findMeetingSlots(timeTable, meetingDuration);
 
     return meetings;
   }
   
   // aggregates a timetable based on the schedule of attendees involved 
-  public void constructTimeTable(Collection<Event> events) {
+  private List<TimeRange> constructTimeTable(Collection<Event> events, Collection<String> attendees) {
+    
+    List<TimeRange> timeTable = new ArrayList<TimeRange>();
 
     // adds up event timeslots where requested attendees are involved in 
     // timeslots are saved in timetable
@@ -81,34 +78,39 @@ public final class FindMeetingQuery {
         };
         i++;
     }
+
+    return timeTable;
   }
 
   // find available meeting timeslots based on the timetable of attendees
-  public void findMeetingSlots(ArrayList<TimeRange> timeTable) {
+  private List<TimeRange> findMeetingSlots(List<TimeRange> timeTable, int meetingDuration) {
+    
+    List<TimeRange> meetings = new ArrayList<TimeRange>();
     int firstStartTime = timeTable.get(0).start();
+
     if (firstStartTime != 0) {
         int start = TimeRange.START_OF_DAY;
         int end = firstStartTime;
-        addMeetingSlot(start, end, false);
+        addMeetingSlot(meetings, start, end, false, meetingDuration);
     }
     if (timeTable.size() > 1) {
         for (int i=0; i < timeTable.size()-1; i++) {
             int start = timeTable.get(i).end();
             int end = timeTable.get(i+1).start();
-            addMeetingSlot(start, end, false);
+            addMeetingSlot(meetings, start, end, false, meetingDuration);
         }
     }
     int lastEndTime = timeTable.get(timeTable.size()-1).end();
     if (lastEndTime != 60*24) {
         int start = lastEndTime;
         int end = TimeRange.END_OF_DAY;
-        addMeetingSlot(start, end, true);
+        addMeetingSlot(meetings, start, end, true, meetingDuration);
     }
-    return;
+    return meetings;
   }
 
   // adds a meeting slot if the timeslot meets requirements
-  public void addMeetingSlot(int start, int end, boolean inclusive) {
+  private void addMeetingSlot(List<TimeRange> meetings, int start, int end, boolean inclusive, int meetingDuration) {
     if (end - start >= meetingDuration) {
         TimeRange timeslot = TimeRange.fromStartEnd(start, end, inclusive);
             meetings.add(timeslot); 
